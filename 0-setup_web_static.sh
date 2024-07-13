8375bcfd62196293db66cb8167a0101edc158592
Ahmed Muawia <amuawia666@gmail.com>
2024-07-13 20:01:30 +0300

    Fixes shellcheck

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,14 +7,14 @@ sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 
-sudo echo "
+echo "
 <html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" > /data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
 sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 
