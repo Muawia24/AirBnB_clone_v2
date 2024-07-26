f6c14e957d333e19f6aea19ec69f00bc40fabce4
Ahmed Muawia <amuawia666@gmail.com>
2024-07-26 20:07:07 +0300

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,13 +3,16 @@
 Hello Flask!
 """
 
+
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run()
