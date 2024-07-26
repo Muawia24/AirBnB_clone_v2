7dbacc2b5d7aabfaf85b08a1e1754741b8ce4c3e
Ahmed Muawia <amuawia666@gmail.com>
2024-07-26 21:39:50 +0300

    4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Hello Flask!
+"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C(text):
+
+    url_txt = text.replace("_", " ")
+    return f'C {url_txt}'
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def py(text='is cool'):
+
+    url_txt = text.replace("_", " ")
+    return f'Python {url_txt}'
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def is_int(n):
+
+    if type(int(n)) == int:
+        return f'{n} is a number'
+
+
+if __name__ == '__main__':
+    app.run()
