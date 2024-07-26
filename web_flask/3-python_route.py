a06d8e152a19e8f9fc4a5956d5b938661161da28
Ahmed Muawia <amuawia666@gmail.com>
2024-07-26 21:09:36 +0300

    3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -11,16 +11,19 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
+
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def C(text):
+
     url_txt = text.replace("_", " ")
     return f'C {url_txt}'
 
@@ -28,8 +31,10 @@ def C(text):
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def py(text='is cool'):
+
     url_txt = text.replace("_", " ")
     return f'Python {url_txt}'
+
 
 if __name__ == '__main__':
     app.run()
