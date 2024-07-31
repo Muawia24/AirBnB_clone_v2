08c8b1aa51db5d370b61446c40bc762c2ca30478
Ahmed Muawia <amuawia666@gmail.com>
2024-08-01 02:10:04 +0300

    PEP8 style

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ 7. Start flask service that does something. """
 
+
 from flask import Flask
 from flask import render_template
 from models import storage
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -15,10 +17,12 @@ def state_list():
     states = sorted(list(storage.all('State').values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
+
 @app.teardown_appcontext
 def teardown_app(exception):
     """ remove the current SQLAlchemy Session """
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
